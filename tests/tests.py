ca82a0785d5cde0631f3be278e1d9fb41b765a3e
Pedro H. Sauné <pedrohsaune@gmail.com>
2025-04-04 18:28:09 -0300

    Test on the `notetofreq` method

--- tests/tests.py
+++ tests/tests.py
@@ -5,9 +5,12 @@ path.append(dirname(path[0]))
 __package__ = "lireta"
 
 from src.audiowave import *
+from src.base import *
 
 (
     (AudioWave().new(1, 330) + AudioWave().new(0.5, 440))
     > AudioWave().new(1, 220)
     > (AudioWave().new(1, 550) + AudioWave().new(2, 275))
 ).play().export_wav("first_test.wav")
+
+print(f"{VoiceThings().notetofreq("A##(+50c)-")} should be equal to 254.177...")
