34062222a1873a5379d784dae80f722fe4fd81d9
Pedro H. Sauné <pedrohsaune@gmail.com>
2025-04-05 11:55:57 -0300

    Initial implementation of `Track` and `Instrument` classes. Move `_get_samples` to the `__init__` of `Track`

--- src/instrument.py
+++ src/instrument.py
@@ -1,13 +1,26 @@
+from enum import Enum
 import scipy.io.wavfile as wavefile
-import sounddevice as sd
 from numpy import average, ndarray
+from math import log
 
 
-def _get_samples(path: str):
-    samplerate, w = wavefile.read(path)
-    if isinstance(w[0], ndarray):
-        # if the audio is stereo, it needs to be converted to mono by averaging
-        wave = [average(i) / 32767 for i in list(w)]
-    else:
-        wave = [i / 32767 for i in list(w)]
-    return samplerate, wave
+class Track:
+    def __init__(self, path, freq):
+        if not path.endswith(".wav"):
+            raise ValueError("Expected a wav file.")
+        self._logfreq = log(freq, 2) / 12
+
+        self._samplerate, w = wavefile.read(path)
+        if isinstance(w[0], ndarray):
+            # if the audio is stereo, it needs to be converted to mono by averaging
+            self._wave = [average(i) / 32767 for i in list(w)]
+        else:
+            self._wave = [i / 32767 for i in list(w)]
+
+
+class Instrument:
+    name: str
+    pitchless: bool
+    continuous: bool
+    interpolation: str
+    tracks: list[Track]
